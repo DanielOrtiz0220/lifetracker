285d3502b2e2adf5983410113a29599ef1acf62e
Daniel O <danielortiz6810@gmail.com>
2022-07-12 16:10:47 -0700

    functional DB working with POST and GET to user, connected to lifetracker DB

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const { PORT } = require("./config");
 const cors = require("cors");
 const morgan = require("morgan");
 const authRoute = require("./routes/auth.js");
+const { NotFoundError } = require("./utils/errors");
 
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
+
 app.use("/auth", authRoute);
 
 app.get("/", (req, res) => {
@@ -19,6 +21,21 @@ app.post("/", (req, res) => {
   res.send(req.body);
 });
 
-app.listen(port, () => {
-  console.log(`🤗 Starter app listening on port ${port}`);
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message;
+  console.log(err);
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`🤗 Starter app listening on port ${PORT}`);
 });
