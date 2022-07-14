8e69ced972e9eaefc7f7c8a936af6e9fe336e774
Daniel O <danielortiz6810@gmail.com>
2022-07-13 18:14:56 -0700

    testing a change

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use("/auth", authRoute);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hola Familia!");
 });
 
 app.post("/", (req, res) => {
