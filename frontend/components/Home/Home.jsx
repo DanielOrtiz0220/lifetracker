4af754a412d727d2a0aa1c269c278eb50a457385
Daniel O <danielortiz6810@gmail.com>
2022-07-14 19:03:22 -0700

    rebasing

--- frontend/components/Home/Home.jsx
+++ frontend/components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import Navbar from "../Navbar/Navbar";
 // import Hero from "../Hero/Hero";
+<<<<<<< HEAD
 import HomeImg from "/Users/daniel.ortiz/Codepath/week5project/lifetracker/frontend/assets/battling-ropes.png";
+=======
+import HomeImg from "/Users/daniel.ortiz/Codepath/week4project/lifetracker(vite)/lifetracker/frontend/assets/battling-ropes.png";
+>>>>>>> 26c6fc3 (Full fledged new frontend)
 import { Grid } from "@mui/material";
 import { Container } from "@mui/material";
 import { Paper } from "@mui/material";
